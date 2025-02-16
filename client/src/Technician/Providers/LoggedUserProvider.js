91109def156a1d08a1ab4efd0fb12ad63753499d
sknefi <filip.karika1@gmail.com>
2025-02-16 15:35:45 +0100

    users, events ADDED, readme.md UPDATED

--- client/src/Technician/Providers/LoggedUserProvider.js
+++ client/src/Technician/Providers/LoggedUserProvider.js
@@ -1,86 +1,86 @@
-import React, { useState } from "react"
+import React, { useState } from "react";
 
-import { LoggedUserContext } from "../Contexts/LoggedUserContext"
-import filip from "../../forApplication/images/userPhotos/filip.jpg"
-import InfoAboutServer from "../InfoAboutServer"
+import { LoggedUserContext } from "../Contexts/LoggedUserContext";
+import filip from "../../forApplication/images/userPhotos/filip.jpg";
+import InfoAboutServer from "../InfoAboutServer";
 
 const LoggedUserProvider = ({ children }) => {
-  const serverInfo = InfoAboutServer()
+  const serverInfo = InfoAboutServer();
 
   const [loadObject, setLoadObject] = useState({
-    state: ""
-  })
+    state: "",
+  });
 
   const [loggedUser, setLoggedUser] = useState(
-  //   {
-  //   name: 'admin',
-  //   surname: 'admin',
-  //   email: 'admin@admin.admin',
-  //   password: 'a',
-  //   igName: 'admin',
-  //   id: '04f0ba2765c2fd8e89d604c0fb7f6bae',
-  //   streak: 89,
-  //   rating: 100,
-  //   rola: 2,
-  //   photo: ''
-  // }
+    //   {
+    //   name: 'admin',
+    //   surname: 'admin',
+    //   email: 'admin@admin.admin',
+    //   password: 'a',
+    //   igName: 'admin',
+    //   id: '04f0ba2765c2fd8e89d604c0fb7f6bae',
+    //   streak: 89,
+    //   rating: 100,
+    //   rola: 2,
+    //   photo: ''
+    // }
 
-  // {
-  //   id: '8b2b893648d34fcc16a46abaf5ed3639',
-  //   name: 'David',
-  //   surname: 'Brown',
-  //   rola: '1',
-  //   email: 'david@example.com',
-  //   password: 'david123',
-  //   streak: 2,
-  //   rating: 5,
-  //   igName: 'david_brown',
-  //   photo: 'david.jpg'
-  // }
+    // {
+    //   id: '8b2b893648d34fcc16a46abaf5ed3639',
+    //   name: 'David',
+    //   surname: 'Brown',
+    //   rola: '1',
+    //   email: 'david@example.com',
+    //   password: 'david123',
+    //   streak: 2,
+    //   rating: 5,
+    //   igName: 'david_brown',
+    //   photo: 'david.jpg'
+    // }
 
-  // {
-  //   name: 'John',
-  //   surname: 'Doe',
-  //   email: 'johndoe@example.com',
-  //   password: 'a',
-  //   igName: 'johndoe_ig',
-  //   photo: 'a',
-  //   id: 'c13b6c6e17b749735950c09e41bd8449',
-  //   streak: 0,
-  //   rating: 0,
-  //   rola: 1
-  // }
+    // {
+    //   name: 'John',
+    //   surname: 'Doe',
+    //   email: 'johndoe@example.com',
+    //   password: 'a',
+    //   igName: 'johndoe_ig',
+    //   photo: 'a',
+    //   id: 'c13b6c6e17b749735950c09e41bd8449',
+    //   streak: 0,
+    //   rating: 0,
+    //   rola: 1
+    // }
 
-  // {
-  //   name: 'Natural',
-  //   surname: 'Tren',
-  //   email: 'aasdasdsd@gmail.com',
-  //   password: 'asd',
-  //   igName: 'naturalizmus',
-  //   id: '6e26bab7d8c576a71cdf876d884ebe53',
-  //   streak: 1,
-  //   rating: 0,
-  //   rola: 1
-  // }
-  
-  {}
-  )
+    // {
+    //   name: 'Natural',
+    //   surname: 'Tren',
+    //   email: 'aasdasdsd@gmail.com',
+    //   password: 'asd',
+    //   igName: 'naturalizmus',
+    //   id: '6e26bab7d8c576a71cdf876d884ebe53',
+    //   streak: 1,
+    //   rating: 0,
+    //   rola: 1
+    // }
 
-  const [userGroups, setUserGroups] = useState([])
+    {}
+  );
+
+  const [userGroups, setUserGroups] = useState([]);
 
   const logout = () => {
-    setLoggedUser( {} )
-  }
+    setLoggedUser({});
+  };
 
   const userIsLoggingIn = (user) => {
-    setLoggedUser(user)
-  }
+    setLoggedUser(user);
+  };
 
   async function findUserInDatabase(email, password) {
     // funtion returns either object of user {id: '...', name: '...', ...}
     // or if user is not in database it will return {} - empty object
     try {
-      setLoadObject((current) => current.state = 'pending')
+      setLoadObject((current) => (current.state = "pending"));
       const response = await fetch(`${serverInfo.gateway}/user/exists`, {
         method: "POST",
         headers: {
@@ -90,133 +90,135 @@ const LoggedUserProvider = ({ children }) => {
           email: email,
           password: password,
         }),
-      })
+      });
 
       if (response.status < 400) {
-        const user = await response.json()
-        setLoadObject((current) => ({ ...current, state: 'ready' }))
+        const user = await response.json();
+        setLoadObject((current) => ({ ...current, state: "ready" }));
         //console.log(user)
 
-        return user
+        return user;
       } else {
-        setLoadObject((current) => ({ ...current, state: 'error' }))
-        throw new Error("Invalid email or password")
+        setLoadObject((current) => ({ ...current, state: "error" }));
+        throw new Error("Invalid email or password");
       }
     } catch (error) {
-      setLoadObject((current) => ({ ...current, state: 'error' }))
-      console.error("Error logging in:", error.message)
+      setLoadObject((current) => ({ ...current, state: "error" }));
+      console.error("Error logging in:", error.message);
     }
   }
 
   async function userRegistration(newUser) {
     try {
-      setLoadObject((current) => ({ ...current, state: 'pending' }))
+      setLoadObject((current) => ({ ...current, state: "pending" }));
       const response = await fetch(`${serverInfo.gateway}/user/create`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(newUser),
-      })
-  
-      if (response.status < 400) {
-        const registeredUser = await response.json()
-        setLoadObject((current) => ({ ...current, state: 'ready' }))
+      });
 
-        return registeredUser
+      if (response.status < 400) {
+        const registeredUser = await response.json();
+        setLoadObject((current) => ({ ...current, state: "ready" }));
+
+        return registeredUser;
       } else {
-        const responseBody = await response.json()
-        if (response.status === 400 && responseBody.code === "emailAlreadyExists") {
+        const responseBody = await response.json();
+        if (
+          response.status === 400 &&
+          responseBody.code === "emailAlreadyExists"
+        ) {
           //console.log('chytil som chybu')
 
-          throw new Error("Email already exists")
+          throw new Error("Email already exists");
         } else {
-          setLoadObject((current) => ({ ...current, state: 'error' }))
-          throw new Error("Failed to register user")
+          setLoadObject((current) => ({ ...current, state: "error" }));
+          throw new Error("Failed to register user");
         }
       }
     } catch (error) {
-      setLoadObject((current) => ({ ...current, state: 'error' }))
-      console.error("Error registering user:", error.message)
+      setLoadObject((current) => ({ ...current, state: "error" }));
+      console.error("Error registering user:", error.message);
     }
   }
 
-
   async function handleInkrementUserStreak() {
-    setLoadObject((current) => ({ ...current, state: "pending" }))
-    
-    console.log(`${serverInfo.gateway}/user/inkrementUserStreak`)
+    setLoadObject((current) => ({ ...current, state: "pending" }));
+
+    //console.log(`${serverInfo.gateway}/user/inkrementUserStreak`)
     const bodyToSend = {
-      "id": loggedUser.id
-    }
-    const response = await fetch(`${serverInfo.gateway}/user/inkrementUserStreak`, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(bodyToSend),
-    })
+      id: loggedUser.id,
+    };
+    const response = await fetch(
+      `${serverInfo.gateway}/user/inkrementUserStreak`,
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(bodyToSend),
+      }
+    );
 
-
-    const responseJson = await response.json()
+    const responseJson = await response.json();
     if (response.status < 400) {
-      setLoadObject((current) => ({ ...current, state: "ready" }))
-      setLoggedUser(responseJson)
+      setLoadObject((current) => ({ ...current, state: "ready" }));
+      setLoggedUser(responseJson);
 
-
-      return responseJson
+      return responseJson;
     } else {
       setLoadObject((current) => ({
         state: "error",
         data: current.data,
         error: responseJson.error,
-      }))
-      throw new Error(JSON.stringify(responseJson, null, 2))
+      }));
+      throw new Error(JSON.stringify(responseJson, null, 2));
     }
   }
-  
+
   async function getUserGroups() {
-    setLoadObject((current) => ({ ...current, state: "pending" }))
-    
-    console.log(`${serverInfo.gateway}/group/getUserGroups`)
+    setLoadObject((current) => ({ ...current, state: "pending" }));
+
+    console.log(`${serverInfo.gateway}/group/getUserGroups`);
 
     const bodyToSend = {
-      "id": loggedUser.id
-    }
+      id: loggedUser.id,
+    };
 
     const response = await fetch(`${serverInfo.gateway}/group/getUserGroups`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(bodyToSend),
-    })
+    });
 
-
-    const responseJson = await response.json()
+    const responseJson = await response.json();
     if (response.status < 400) {
-      setLoadObject((current) => ({ ...current, state: "ready" }))
-      setUserGroups(responseJson)
+      setLoadObject((current) => ({ ...current, state: "ready" }));
+      setUserGroups(responseJson);
 
-      return responseJson
+      return responseJson;
     } else {
       setLoadObject((current) => ({
         state: "error",
         data: current.data,
         error: responseJson.error,
-      }))
-      throw new Error(JSON.stringify(responseJson, null, 2))
+      }));
+      throw new Error(JSON.stringify(responseJson, null, 2));
     }
   }
-  
+
   async function handleFileUpload(file, newUserId) {
     setLoadObject((current) => ({ ...current, state: "pending" }));
     const formData = new FormData();
     formData.append("photo", file);
     formData.append("id", newUserId);
 
-    console.log(formData)
-  
+    console.log(formData);
+
     try {
       const response = await fetch(`${serverInfo.gateway}/user/uploadPhoto`, {
         method: "POST",
         body: formData,
       });
-  
+
       if (response.status < 400) {
         const responseData = await response.json();
         setLoadObject((current) => ({ ...current, state: "ready" }));
@@ -230,8 +232,6 @@ const LoggedUserProvider = ({ children }) => {
       console.error("Error uploading file:", error.message);
     }
   }
-  
-  
 
   const value = {
     state: loadObject.state,
@@ -240,14 +240,14 @@ const LoggedUserProvider = ({ children }) => {
     handlerMapForLogin: { logout, userIsLoggingIn, findUserInDatabase },
     handlerMapForRegistration: { userRegistration, handleFileUpload },
     handlerMapForUserUpdate: { handleInkrementUserStreak },
-    handlerMapForUserGroups: { getUserGroups }
-  }
+    handlerMapForUserGroups: { getUserGroups },
+  };
 
   return (
     <LoggedUserContext.Provider value={value}>
       {children}
     </LoggedUserContext.Provider>
-  )
-}
+  );
+};
 
-export default LoggedUserProvider
+export default LoggedUserProvider;
---
+++ readme.md
@@ -0,0 +1,69 @@
+# Meet Us
+
+## O aplikácii
++ Prisťahovali ste sa do mesta a chcete spoznať niekoho so spoločnými záujmami?
++ Chystáte sa na party do klubu, ale nemáte nikoho s kým by ste išli?
++ Chcete sa prihlásiť na amatérsky turnaj vo volejbale, ale nemáte hráčov do tímu?
++ Plánujete sa zúčastniť letného festivalu v zahraničí, ale nechcete ísť sám?
+
+Potom je tu riešenie - `Meet Us`, aplikácia ktorá spája ľudí
+
+## Používanie aplikácie
+### 3 pohlady - public, user, admin
++ `public`  -> nemôže využívať funkcionality aplikácie (iba login/registration)
++ `user`    -> vytvoriť skupinu, pridať sa do skupiny, zobraziť všetky skupiny, v ktorých sa nachádza, logout
++ `admin`   -> všetko čo môže user + vytvoriť event
+
+### Registrácia
+Vytvorenie účtu pre `public`
+
+### Login
+Prihlásenie sa s validnými prihlasovacími údajmi
+
+### Darkmode
+Možnosť farebne si prispôsobiť aplikáciu
+
+### Ohník
+Číslo pod ikonkou  nám hovorí v koľkých skupinách je/bol daný `user`
+
+### Číslo pod ikonkou 'hviezda' nám hovorí aký rating má user
++ `rating` - čím väčší rating tým je user sympatickejší
++ funkcionalita rating zatiaľ nebola v aplikácii implementovaná
++ na konci eventu môže user ohodnotiť členov skupiny, s ktorými
++ bol na aplikácii 
+	+ `rating` -> inkrement hodnoty
+	+ `rating` -> dekrement hodnoty
+    
+## Aplikácia z technickej stránky
+Pri zmene `server gateway` je nutné prenastaviť túto hodnotu aj v `/client/src/Technician/InfoAboutServer.js`
+    
+Ide o môj prvý väčší projekt, po jeho dokončení som si uvedomil, že kód sa dal napísať lepšie
+
+Čo ma projekt naučil alebo čo by som zmenil na tomto projekte keby som ho vytváral od začiatku:
+
+### Frontend
++ kód by som rozdelil zmysluplnejšie do komponent
++ každý modal by bol samostatná komponenta
++ použiť zmysluplnejšie názvy súborov (`Technician`, `UserCardF` - F ako folder, `NavbarF`, ...)
++ nájsť lepší spôsob akým implementovať `Darkmode` (kód v niektorých častiach vyzeral divoko - `client/src/forApplication/CreateNewGroup/AddGroup.js`)
+### Backend
++ na strane servera by som spravil storage ako samostatnú zložku (`/storage`) a nie ako podzložku Dao (`Dao/storage`)
++ jedným z cieloch projektu bolo naučiť sa pracovať so súbormi (`/storage`), v budúcnosti radšej implementovať databázu
++ zabezpečiť hashovanie hesla, aby pri registrácii nešlo otvorené heslo od clienta serveru
+
+## Záver
++ Podarilo sa mi vytvoriť funkčú aplikáciu kde komunikuje client (`React JS`) a server (`Express JS`)
++ Projekt mi dal mnoho skúseností a ukázal mi, že stále sa je čo učiť a existuje stále kopa veci v čom sa zdokonaľovať
++ Projekt mi poskytol základnú kostru vytvárania aplikácii, v budúcnosti budem vedieť čo a ako spraviť lepšie, čomu sa vyhnúť a čo spraviť inak
++ Aj keď projekt nie je 100% perfektný slúži pre mňa ako odrazový mostík do sveta webdevu...
+
+## Spustenie aplikácie
+Spustenie `serveru` (uistite sa, že ste v zložke ./server)
+```
+npm run devStart
+```
+Spustenie `clienta` (uistite sa, že ste v zložke ./client)
+```
+npm start
+```
+Užite si aplikáciu 🙂
